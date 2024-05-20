d546d697bd3909fa883657fadc6e6f4c874b4af8
Billyhyunjun <phjphj9999@gmail.com>
2024-05-20 11:44:06 +0900

    chore : AI 음성인식 기능
    AI 음성인식 기능 구현을 위해 views.py와 AIbot.py 연동 확인

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from .AIbot import AI
+import time
 
-# Create your views here.
+import speech_recognition as sr
+from rest_framework.views import APIView
+
+
+
+class AIbot(APIView):
+    def post(self, request):
+        r = sr.Recognizer()
+        m = sr.Microphone()
+
+        AI.speak("무엇을 도와드릴까요?")
+        stop_listening = r.listen_in_background(m, AI.listen)  # 처음 인자는 입력 신호, 두번 째 인자는 실행을 하는 함수
+
+        while True:
+            time.sleep(0.1)
