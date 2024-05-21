734234905bd630d32c1a0c7ddcf43ad201674420
Jenhynn <159970972+Jenhynn@users.noreply.github.com>
2024-05-21 16:17:54 +0900

    feat: Hashtag 기능 구현
    1. 해시태그 CRUD admin 페이지에서 실행 구현, superuser, staff 권한 별도 부여
    - superuser: 모든 해시태그에 대해 CRUD 가능
    - staff: 본인이 작성한 해시태그에 대해서만 CRUD 가능
    2. 해시태그 생성 시 작성 유저의 id와 username 반영
    - readonly_fields로 조회만 가능
    3. 해시태그 필터 기능
    - superuser: 작성자 username에 따라 필터 적용 가능
    - staff: 해시태그에 따라 필터 적용 가능

--- menus/admin.py
+++ menus/admin.py
@@ -11,10 +11,19 @@ class MenuAdmin(admin.ModelAdmin):
     # date_hierarchy = "created_at"
     # ordering = ("-created_at",)
 
+    def get_form(self, request, obj, **kwargs):
+        form = super(MenuAdmin, self).get_form(request, obj, **kwargs)
+        print("form>>>>>>>>>>>", form)
+        print("dir>>>>>>>>>>>>>>>", dir(form)) #### 내부를 볼 수 있음..!!
+        print("base_fields >>>>>>>>>> ", form.base_fields) 
+        if request.user.is_superuser:
+            form.base_fields['hashtags'].queryset = Hashtag.objects.all()
+        elif request.user.is_staff:
+            form.base_fields['hashtags'].queryset = Hashtag.objects.filter(hashtag_author_id=request.user)
+        return form
+
     def save_model(self, request, obj, form, change):
         obj.store = request.user
-        # store_id = obj.store.store_name
-        # obj.store.store_id = request.user.id
         super().save_model(request, obj, form, change)
 
     def get_queryset(self, request):
@@ -35,35 +44,19 @@ class MenuAdmin(admin.ModelAdmin):
 class HashtagAdmin(admin.ModelAdmin):
     readonly_fields = ("menu_id", "menu", "hashtag_author")
 
-    # def get_queryset(self, request):
-    #     print("들어오는 REQUEST >>>>>> ", request)
-    #     qs = super().get_queryset(request)
-    #     if request.user.is_superuser:
-    #         return qs
-    #     return qs.filter(menu_id=request.user)
-    
-    # def get_list_filter(self, request):
-    #     if request.user.is_superuser:
-    #         list_filter = ("menu_id",)
-    #     else:
-    #         list_filter = ("hashtags",)
-    #     return list_filter
-    
+    def get_search_results(self, request, queryset, search_term):
+        print("get_search_results >>>>>>>> 들어오는지")
+        queryset = queryset.filter(hashtag_author_id=request.user)
+        qs = super().get_search_results(request, queryset, search_term)
+        print(qs)
+        return qs
 
-## hashtag 는 menu_id 가 필요 -> 해당 menu를 작성한 유저만 해시태그를 조회할 수 있도록 해야함
-## hashtag -> menu -> user. 메뉴는 유저마다 다르기 때문에 요청한 유저만 자신이 작성한 해시태그를 CRUD 가능
+    def save_model(self, request, obj, form, change):
+        obj.hashtag_author = request.user
+        super().save_model(request, obj, form, change)
 
-
-
-
-#### hashtag 만들 때 반드시 메뉴가 있어야 하나? -> 해시태그를 미리 만들어 놓고 메뉴를 생성할 때 선택하게
-#### 메뉴를 생성할 때는 해시태그 필수 -> 음성인식이 가능하도록
-
-
-#### add menu 할 때 태그까지 선택해서 넣기
-#### hashtag 입력할 때 menu를 dropdown으로 선택하기
-
-### 1. menu를 blank=True
-### 2. menu와 연관된 User 모델.. Hashtag - User : 해시태그를 만든 유저. 가 될 것. 
-### 3. User 1 - 커피 라는 해시태그, User2 - 커피 라는 해시태그를 만들 때 같은 커피로 인식이 되나 ?
-### 지금 pk, menu, hashtag -> 작성한 사람 X:
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(hashtag_author_id=request.user)
