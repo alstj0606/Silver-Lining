f62c543f035a05ba8e5575b40d331d6676d8e398
Billyhyunjun <phjphj9999@gmail.com>
2024-05-21 19:22:13 +0900

    fix : dev marge 충돌 수정
    merge를 위한 수정

--- menus/admin.py
+++ menus/admin.py
@@ -5,17 +5,18 @@ from .models import Hashtag, Menu
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
     readonly_fields = ("store", "store_id")
-    list_display = ("food_name", "price")
+    list_display = ("food_name", "price", "get_hashtags")
     search_fields = ("food_name", "price")
-    # list_filter = ("name", "user")
+
+    # list_filter = ("hashtags",)
     # date_hierarchy = "created_at"
     # ordering = ("-created_at",)
 
     def get_form(self, request, obj, **kwargs):
         form = super(MenuAdmin, self).get_form(request, obj, **kwargs)
-        print("form>>>>>>>>>>>", form)
-        print("dir>>>>>>>>>>>>>>>", dir(form)) #### 내부를 볼 수 있음..!!
-        print("base_fields >>>>>>>>>> ", form.base_fields) 
+        # print("form>>>>>>>>>>>", form)
+        # print("dir>>>>>>>>>>>>>>>", dir(form))  #### 내부를 볼 수 있음..!!
+        # print("base_fields >>>>>>>>>> ", form.base_fields)
         if request.user.is_superuser:
             form.base_fields['hashtags'].queryset = Hashtag.objects.all()
         elif request.user.is_staff:
@@ -31,18 +32,25 @@ class MenuAdmin(admin.ModelAdmin):
         if request.user.is_superuser:
             return qs
         return qs.filter(store_id=request.user)
-    
+
     def get_list_filter(self, request):
         if request.user.is_superuser:
             list_filter = ("store_id",)
         else:
             list_filter = ("hashtags",)
         return list_filter
-    
+
+    def get_hashtags(self, obj):
+        return ", ".join([hashtag.hashtag for hashtag in obj.hashtags.all()])
+
+    get_hashtags.short_description = "Hashtags"
+
 
 @admin.register(Hashtag)
 class HashtagAdmin(admin.ModelAdmin):
-    readonly_fields = ("menu_id", "menu", "hashtag_author")
+    readonly_fields = ("hashtag_author",)
+    list_display = ("hashtag", "hashtag_author", "get_menus")
+    list_filter = ("hashtag_author",)
 
     def save_model(self, request, obj, form, change):
         obj.hashtag_author = request.user
@@ -52,4 +60,9 @@ class HashtagAdmin(admin.ModelAdmin):
         qs = super().get_queryset(request)
         if request.user.is_superuser:
             return qs
-        return qs.filter(hashtag_author_id=request.user)
+        return qs.filter(hashtag_author=request.user)
+
+    def get_menus(self, obj):
+        return ", ".join([menu.food_name for menu in obj.menu_items.all()])
+
+    get_menus.short_description = "Menus"
