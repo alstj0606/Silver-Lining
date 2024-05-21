4bc83fc3ec71145b856aad78405af47e3e67bb58
Jenhynn <159970972+Jenhynn@users.noreply.github.com>
2024-05-21 12:10:34 +0900

    feat:accounts, menus admin에 기능 추가
    1. 유저 CRUD, 메뉴 CRUD 권한 superuser, staff에 권한 별도로 부여 및 실행 확인
    - superuser: 유저 CRUD, 메뉴 CRUD 가능
    - staff: 메뉴 CRUD 가능
    2. 유저 생성 시 Custom User Admin 사용해서 조회 필드와 필드 순서 커스텀
    3. User 모델, Menu 모델, Hashtag 모델 admin에 등록
    4. 필터 조회를 superuser, staff에 권한 별도로 부여
    - superuser: 모든 필터 조회 가능
    - staff: 해시태그 필터만 조회 가능
    5. staff가 메뉴 생성 시 store의 username, id 는 수정 불가

--- menus/admin.py
+++ menus/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Hashtag, Menu
 
-# Register your models here.
+admin.site.register(Hashtag)
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    readonly_fields = ("store", "store_id")
+    list_display = ("food_name", "price")
+    search_fields = ("food_name", "price")
+    # list_filter = ("name", "user")
+    # date_hierarchy = "created_at"
+    # ordering = ("-created_at",)
+
+    def save_model(self, request, obj, form, change):
+        obj.store = request.user
+        # store_id = obj.store.store_name
+        # obj.store.store_id = request.user.id
+        super().save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(store_id=request.user)
+    
+    def get_list_filter(self, request):
+        if request.user.is_superuser:
+            list_filter = ("store_id",)
+        else:
+            list_filter = ("hashtags",)
+        return list_filter
