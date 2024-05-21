9d9fa12e6e3be66eaf25f9e4ab68e3d9629ec4d0
Billyhyunjun <phjphj9999@gmail.com>
2024-05-21 15:15:13 +0900

    chore : 키오스크 주문 화면 전환
    키오스크에서 주문된 데이터를 views.py에 입력을 받고 나온 결과를 다시 template에 전송하여 주문결과 화면으로 이동 및 번호 출력

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from .bot import bot
 from django.http import JsonResponse
-
+import json
 
 def kiosk_view(request):
     # kiosk.html 템플릿을 렌더링하여 사용자에게 보여줌
@@ -25,17 +25,17 @@ class AIbot(APIView):
 
 def submit_order(request):
     if request.method == 'POST':
-        # 주문을 제출하는 POST 요청을 처리합니다.
-        # 요청에서 데이터를 추출합니다.
-        # 주문을 데이터베이스에 저장합니다.
-        # 예시를 위해, 'name'과 'price'를 POST 데이터에서 가져오는 것으로 가정합니다.
-        name = request.POST.get('name')
-        price = request.POST.get('price')
-        # 주문 객체를 생성하거나 가져옵니다.
-        # order, created = Order.objects.get_or_create(name=name, defaults={'price': price})
-        order_number = 100
-        # 주문 완료 페이지로 주문 번호와 함께 리다이렉트합니다.
-        return redirect('order_complete', order_number=order_number)
+        data = json.loads(request.body)
+        selected_items = data.get('items', [])
+        total_price = data.get('total_price', 0)
+
+        print(selected_items)
+        print(total_price)
+
+        # 여기서 선택된 상품들과 총 금액을 이용하여 주문을 처리하고, 주문 번호를 생성합니다.
+        order_number = 404
+
+        return JsonResponse({'order_number': order_number})
 
 
 def order_complete(request, order_number):
