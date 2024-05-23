6f800edd4cd4028adb4c8fc5ec4e4bd99eb733f2
Jenhynn <159970972+Jenhynn@users.noreply.github.com>
2024-05-23 09:42:40 +0900

    refactor: gpt-4o 프롬프트와 코드 일부 수정
    - 기존: gpt-4o의 나이값 응답에서 숫자 뒤에 s 까지 붙어나와 s와 숫자를 분리해주는 코드와 관련 패키지가 더 필요했던 문제
    - 해결: 나이값 응답에서 숫자만 받아올 수 있도록 수정해 불필요한 코드를 삭제

--- orders/views.py
+++ orders/views.py
@@ -13,7 +13,6 @@ from django.views.decorators.csrf import csrf_exempt
 from .models import Order
 from menus.models import Menu
 import os
-import re
 
 
 def kiosk_view(request):
@@ -114,9 +113,6 @@ def face_recognition(request):
     # 프레임 읽기
     while True:
         ret, frame = cap.read()
-        print("ret>>>>>>>>>>>", ret)
-        print("frame>>>>>>>>>", frame)
-
 
         if not ret:
             raise Exception("Cannot read frame from webcam")
@@ -125,23 +121,19 @@ def face_recognition(request):
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
         if faces.any():
-            print("faces>>>>>>>", faces)
             break
 
     cap.release()
 
     image_path = "face.jpg"
-    print("image_path>>>>>>>>>>", image_path)
     cv2.imwrite(image_path, frame)
 
 
     with open(image_path, "rb") as image_file:
         encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
-        print("encoded_image>>>>>>>>>>>", encoded_image)
+
     # base64_image = encoded_image(image_path)
     base64_image = f"data:image/jpeg;base64,{encoded_image}"
-
-    print("base64_image>>>>>>>>>", base64_image)
 
     headers = {
     "Content-Type": "application/json",
@@ -156,7 +148,7 @@ def face_recognition(request):
         "content": [
             {
             "type": "text",
-            "text": "Although age can be difficult to predict, please provide an approximate number for how old the person in the photo appears to be. Please consider that Asians tend to look younger than you might think.And Please provide an approximate age in 10-year intervals such as teens, 20s, 30s, 40s, 50s, 60s, 70s, or 80s. Please return the inferred age in the format 'Estimated Age: [inferred age]'."
+            "text": "Although age can be difficult to predict, please provide an approximate number for how old the person in the photo appears to be. Please consider that Asians tend to look younger than you might think.And Please provide an approximate age in 10-year intervals such as teens, 20s, 30s, 40s, 50s, 60s, 70s, or 80s. When you return the value, remove the 's' in the end of the age interval. For example, when you find the person to be in their 20s, just return the value as 20. Please return the inferred age in the format 'Estimated Age: [inferred age]'."
             },
             {
             "type": "image_url",
@@ -169,7 +161,7 @@ def face_recognition(request):
     ],
     "max_tokens": 300
     }
-    print("api통과??>>>>>>>>>>>>")
+
     response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
 
     try:
@@ -182,10 +174,8 @@ def face_recognition(request):
     ai_answer = response.json()
 
     age_message = ai_answer["choices"][0]['message']['content']
-
     age = age_message.split("Estimated Age: ")[1].strip()
-    number = re.findall(r'\d+', age)
-    age_number = int(number[0])
+    age_number = int(age)
 
     if age_number >= 60:
         return render(request, 'orders/menu_big.html')
