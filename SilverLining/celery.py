9e1a8340e66c354b7b6afd5aeb461e47aa433454
Billyhyunjun <phjphj9999@gmail.com>
2024-06-03 12:41:34 +0900

    feat: docker 테스트
    Docker를 통해 외부 기능을 사용할 수 있도록 구현

---
+++ SilverLining/celery.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SilverLining.settings')
+app = Celery('SilverLining')
+
+# 문자열로 등록은 Celery Worker가 자식 프로세스에게 피클링하지 하지 않아도 되다고 알림
+# namespace = 'CELERY'는 Celery관련 세팅 파일에서 변수 Prefix가 CELERY_ 라고 알림
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
