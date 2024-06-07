52b3623b1b425acf13871b50dc35070a4f027a7e
Billyhyunjun <phjphj9999@gmail.com>
2024-06-07 09:47:11 +0900

    merge: elder_cart 와 충돌해결
    merge시 elder_cart와 충돌되는 부분을 해결

--- SilverLining/settings.py
+++ SilverLining/settings.py
@@ -106,16 +106,6 @@ DATABASES = {
 }
 
 CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://redis:6379/1',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
-    }
-}
-
-CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": "redis://127.0.0.1:6379",  # Redis 서버의 위치
