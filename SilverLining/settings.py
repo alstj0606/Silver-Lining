9a0d3ed678293a19ab9e9269bec16ca44817c1cc
Billyhyunjun <phjphj9999@gmail.com>
2024-06-10 13:02:35 +0900

    merge: static 파일 수정
    static 파일 오류 수정

--- SilverLining/settings.py
+++ SilverLining/settings.py
@@ -171,10 +171,8 @@ LANGUAGES = [
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / 'static'
 
-# Media files
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
